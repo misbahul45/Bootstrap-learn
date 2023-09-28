39b0230666353116a3189c35c6bbda6f014e327c
misbahul45 <misbahulmuttaqin395@gmail.com>
2023-09-28 16:23:41 +0700

    push

--- React js/documentasion-react/learn1/src/components/footer.js
+++
@@ -1,8 +0,0 @@
-function footer({length}) {
-    return (
-      <footer className=" text-center absolute bottom-0 left-0 w-full bg-blue-500 py-5">
-        <p className=" font-bold text-red-200 text-sm ">{length === 1 ?"item":"items"}</p>
-      </footer>
-    );
-  }
-export default footer;
